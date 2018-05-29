f9ec334006523ed7ad7c6617750ef2f8b649ed70
datsgawas <dattaram.gawas@metamagic.in>
2018-05-29 18:08:34 +0530

    Home : Auth guard updated.

--- src/app/auth-service/home.auth.service.ts
+++ src/app/auth-service/home.auth.service.ts
@@ -10,11 +10,13 @@ import {CookieService} from "ngx-cookie-service";
 export class HomeAuthService implements CanLoad {
   constructor(private _cookieService: CookieService, private _route: Router) {}
   canLoad(route: Route): Observable<boolean> | Promise<boolean> | boolean {
+    if (this._cookieService.check('emailId') && this._cookieService.check('password')) {
       if (route.data[0] == this._cookieService.get('emailId') && route.data[1] == this._cookieService.get('password')) {
         return true;
       } else {
         this._route.navigate(['login']);
         return false;
       }
+    }
   }
 }
